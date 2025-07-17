f93570b4f776910f5831b589f85f2a4727316c6c
Gabriel Molina <g-abox@hotmail.com>
2025-07-17 14:45:46 -0300

    Refactorize path definitions

--- src/jobnlp/pipeline/fetch_raw.py
+++ src/jobnlp/pipeline/fetch_raw.py
@@ -1,18 +1,24 @@
-import os, yaml, pathlib
+import yaml, pathlib
 
-from scraper.sites.classif_ads import NewsPapAds
-from utils import logger 
+import jobnlp
+from jobnlp.scraper.sites.classif_ads import NewsPapAds
+from jobnlp.utils import logger 
 
 
 def load_config():
-    with open(os.path.join("config", "scraper.yml"), "r", encoding="utf-8") as f:
+    CFG_PATH = (
+        pathlib.Path(jobnlp.__file__)
+        .resolve()
+        .parent / "scraper" / "config" / "scraper.yml"
+    )
+    with open(CFG_PATH, "r", encoding="utf-8") as f:
         cfg = yaml.safe_load(f)     
 
     return cfg
 
 if __name__ == "__main__":
-    LOG_PATH = pathlib.Path("log/main.log")
-    LOG_PATH.parent.mkdir(parents=True, exist_ok=True)
+    LOG_PATH = pathlib.Path("log/fetch_raw.log")
+    
     logger.setup_logging(logfile=LOG_PATH)
 
     classif = NewsPapAds(config=load_config())
