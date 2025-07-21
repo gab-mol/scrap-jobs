d107b40588f8e9b9c0b1d95aea4af36e7047b56b
Gabriel Molina <g-abox@hotmail.com>
2025-07-21 18:58:16 -0300

    record insertions

--- src/jobnlp/db/models.py
+++ src/jobnlp/db/models.py
@@ -1,11 +1,11 @@
 from psycopg2.errors import OperationalError
-
+from typing import Literal
 from jobnlp.db.connection import conn
 from jobnlp.utils.logger import get_logger
 
 log = get_logger(__name__)
 
-def insert_bronze(add: dict) -> None:
+def insert_bronze(add: dict) -> Literal[0, 1]:
     '''
     Insert row into table `adds_bronze`.
 
@@ -18,18 +18,21 @@ def insert_bronze(add: dict) -> None:
     '''
     query = """INSERT INTO adds_lakehouse.adds_bronze (scrap_date, source_url, norm_text, hash)
                     VALUES (%s, %s, %s, %s)
-                    ON CONFLICT (hash) DO NOTHING;
+                    ON CONFLICT (hash) DO NOTHING
+                    RETURNING id;
                 """
     try:
-        with conn.cursor() as cur:
-                cur.execute(query, (
-                    add["scrap_date"],
-                    add["source_url"],
-                    add["norm_text"],
-                    add["hash"]
-                ))
-
+        cur = conn.cursor()
+        cur.execute(query, (
+            add["scrap_date"],
+            add["source_url"],
+            add["norm_text"],
+            add["hash"]
+        ))
+        inserted = cur.fetchone()
+        cur.close()
         conn.commit()
+        return 1 if inserted else 0
     except OperationalError:
         log.error(f"Error inserting record with hash: {add['hash']}")
         raise
