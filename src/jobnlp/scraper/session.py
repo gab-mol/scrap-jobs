112d255526b0103fd8111027ab97dfccf93ceb2a
Gabriel Molina <g-abox@hotmail.com>
2025-07-22 11:31:53 -0300

    replaced the provisory URL with the actual repository URL

--- src/jobnlp/scraper/session.py
+++ src/jobnlp/scraper/session.py
@@ -4,7 +4,7 @@ from urllib3.util import Retry
 
 def build_session(
     retries=3, backoff=0.5,
-    ua="job-scraper/1.0 (+https://github.com/gab-mol/scraper_jobs)",
+    ua="job-scraper/1.0 (+https://github.com/gab-mol/scrap-jobs.git)",
 ):
     retry = Retry(
         total=retries, backoff_factor=backoff,
