a1ea7f8f0a9050de2d7a9ed74d9ede4d2a8db009
Gabriel Molina <g-abox@hotmail.com>
2025-07-22 13:52:07 -0300

    improve user agent

--- src/jobnlp/scraper/session.py
+++ src/jobnlp/scraper/session.py
@@ -4,7 +4,7 @@ from urllib3.util import Retry
 
 def build_session(
     retries=3, backoff=0.5,
-    ua="job-scraper/1.0 (+https://github.com/gab-mol/scrap-jobs.git)",
+    ua="scrap-jobs/0.1.0 (+https://github.com/gab-mol/scrap-jobs.git)",
 ):
     retry = Retry(
         total=retries, backoff_factor=backoff,
