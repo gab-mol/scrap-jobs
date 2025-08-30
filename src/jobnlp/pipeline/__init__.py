7c8caaf3037caa73753017b5e9985eb257c30f82
Gabriel Molina <g-abox@hotmail.com>
2025-08-30 18:08:21 -0300

    expose pipeline tasks

---
+++ src/jobnlp/pipeline/__init__.py
@@ -0,0 +1,11 @@
+from . import fetch_raw
+from . import clean_text
+from . import nlp_extract
+from . import entity_count
+
+__all__ = [
+    "fetch_raw",
+    "clean_text",
+    "nlp_extract",
+    "entity_count",
+]
