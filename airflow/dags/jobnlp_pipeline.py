3310b6d6088e9bb1795e7118ac5dd7decf77f874
Gabriel Molina <g-abox@hotmail.com>
2025-09-08 08:34:58 -0300

    Remove the responsibility of starting and stopping the lakehouse container from the pipeline

--- airflow/dags/jobnlp_pipeline.py
+++ airflow/dags/jobnlp_pipeline.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime
-from jobnlp.pipeline import pipeline_tasks
+import jobnlp.pipeline as pipeline_tasks
 
 default_args = {
     'owner': 'airflow',
@@ -17,16 +17,6 @@ with DAG(
     start_date=datetime(2025, 8, 13),
     catchup=False,
 ) as dag:
-    load_env = PythonOperator(
-        task_id='load_env',
-        python_callable=pipeline_tasks.load_env,
-    )
-
-    start_docker = PythonOperator(
-        task_id='start_docker',
-        python_callable=pipeline_tasks.start_docker_compose,
-    )
-
     fetch_raw = PythonOperator(
         task_id='fetch_raw',
         python_callable=pipeline_tasks.fetch_raw.main,
@@ -47,9 +37,4 @@ with DAG(
         python_callable=pipeline_tasks.entity_count.main,
     )
 
-    stop_docker = PythonOperator(
-        task_id='stop_docker',
-        python_callable=pipeline_tasks.stop_docker_compose,
-    )
-
-    load_env >> start_docker >> fetch_raw >> clean_text >> nlp_extract >> entity_count >> stop_docker
+    fetch_raw >> clean_text >> nlp_extract >> entity_count
